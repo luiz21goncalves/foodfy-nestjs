2e5e7be9084cda82bcb88657c641bf5424833cd4
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2024-08-03 11:32:15 -0300

    chore: run lint and format scripts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
