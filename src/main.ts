3e9f56276e6ab0e8df34cb9569080d601f4ff67a
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2025-02-23 17:04:36 -0300

    feat: add api docs (#1)

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { VersioningType } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 import { Env } from './env';
@@ -14,6 +15,16 @@ async function bootstrap() {
 
   const configServise = app.get<ConfigService<Env, true>>(ConfigService);
   const port = configServise.get('PORT', { infer: true });
+
+  const config = new DocumentBuilder()
+    .setTitle('Foodfy NestJS')
+    .setDescription(
+      'Aprenda a construir os melhores pratos com receitas criadas por profissionais do mundo inteiro.',
+    )
+    .setVersion(process.env.npm_package_version!)
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/docs', app, document);
 
   await app.listen(port);
 }
