0580599e048d84d5a8cc36459b2be876641cd454
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2025-02-23 12:26:51 -0300

    style: ignore coverage directory

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,7 +7,7 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['eslint.config.mjs', 'node_modules/*', 'dist/*'],
+    ignores: ['eslint.config.mjs', 'node_modules/*', 'dist/*', 'coverage/*'],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
