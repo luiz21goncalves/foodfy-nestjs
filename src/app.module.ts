77d29107c6f7e02522fe97ec0861108cf6bfa3d8
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2025-02-23 10:41:41 -0300

    feat: add typeorm module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Env } from './env';
 
 @Module({
   controllers: [AppController],
@@ -10,6 +12,31 @@ import { AppService } from './app.service';
     ConfigModule.forRoot({
       expandVariables: true,
       isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory(configService: ConfigService<Env, true>) {
+        const host = configService.get('POSTGRES_HOST', { infer: true });
+        const port = configService.get('POSTGRES_PORT', { infer: true });
+        const username = configService.get('POSTGRES_USER', {
+          infer: true,
+        });
+        const password = configService.get('POSTGRES_PASSWORD', {
+          infer: true,
+        });
+        const database = configService.get('POSTGRES_DB', { infer: true });
+
+        return {
+          autoLoadEntities: true,
+          database,
+          host,
+          password,
+          port,
+          type: 'postgres',
+          username,
+        };
+      },
     }),
   ],
   providers: [AppService],
--- src/env.ts
+++ src/env.ts
@@ -3,9 +3,19 @@ import * as Joi from 'joi';
 export type Env = {
   NODE_ENV: 'test' | 'development' | 'production';
   PORT: number;
+  POSTGRES_HOST: string;
+  POSTGRES_USER: string;
+  POSTGRES_PASSWORD: string;
+  POSTGRES_DB: string;
+  POSTGRES_PORT: number;
 };
 
 export const envSchema = Joi.object<Env, true>({
   NODE_ENV: Joi.string().valid('test', 'development', 'production'),
   PORT: Joi.number().port(),
+  POSTGRES_DB: Joi.string().required(),
+  POSTGRES_HOST: Joi.string().required(),
+  POSTGRES_PASSWORD: Joi.string().required(),
+  POSTGRES_PORT: Joi.number().port(),
+  POSTGRES_USER: Joi.string().required(),
 });
