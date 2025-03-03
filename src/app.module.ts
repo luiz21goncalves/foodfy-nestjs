5197138ad5291344349076a1a4ecf658bf1a4c03
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2025-03-03 10:18:49 -0300

    feat: add environment variables validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
+import { envSchema } from './env';
 import { StatusModule } from './v1/status/status.module';
 
 @Module({
@@ -9,6 +10,7 @@ import { StatusModule } from './v1/status/status.module';
       envFilePath: [`.env.${process.env.NODE_ENV}`],
       expandVariables: true,
       isGlobal: true,
+      validationSchema: envSchema,
     }),
     StatusModule,
   ],
