80c7e42e95048efcb68f269fefcaeee997052c4b
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2025-03-03 10:20:29 -0300

    feat: add prisma

--- src/v1/status/status.controller.ts
+++ src/v1/status/status.controller.ts
@@ -1,13 +1,25 @@
 import { Controller, Get } from '@nestjs/common';
-import { HealthCheck, HealthCheckService } from '@nestjs/terminus';
+import {
+  HealthCheck,
+  HealthCheckService,
+  PrismaHealthIndicator,
+} from '@nestjs/terminus';
+
+import { PrismaService } from '@/prisma/prisma.service';
 
 @Controller({ path: 'status', version: '1' })
 export class StatusController {
-  constructor(private readonly health: HealthCheckService) {}
+  constructor(
+    private readonly health: HealthCheckService,
+    private readonly prismaHeath: PrismaHealthIndicator,
+    private readonly prisma: PrismaService,
+  ) {}
 
   @Get()
   @HealthCheck()
   check() {
-    return this.health.check([]);
+    return this.health.check([
+      () => this.prismaHeath.pingCheck('postgres', this.prisma),
+    ]);
   }
 }
